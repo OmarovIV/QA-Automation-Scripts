f18656237c5be74ba35cec74b15a8eaeffeff66a
OmarovIV <xyarriddi@gmail.com>
2024-10-14 14:26:04 +0200

    refactor tests

--- tests/sauce-login.spec.ts
+++ tests/sauce-login.spec.ts
@@ -1,12 +1,17 @@
 import { expect, test } from "@playwright/test";
+import { login } from "./helpers/loginHelper";
 
 test.describe("Sauce Demo Login", () => {
   test("should fail to log in with incorrect credentials", async ({ page }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.fill('input[id="user-name"]', "invalid_user");
-    await page.fill('input[id="password"]', "invalid_password");
-    await page.click('input[id="login-button"]');
-
+    await login(
+      page,
+      "invalid_user",
+      "invalid_password",
+      "https://www.saucedemo.com/",
+      'input[id="user-name"]',
+      'input[id="password"]',
+      'input[id="login-button"]'
+    );
     const errorMessage = await page.locator('h3[data-test="error"]');
     await expect(errorMessage).toContainText(
       "Epic sadface: Username and password do not match"
@@ -16,11 +21,15 @@ test.describe("Sauce Demo Login", () => {
   test("should log in successfully with valid credentials", async ({
     page,
   }) => {
-    await page.goto("https://www.saucedemo.com/");
-    await page.fill('input[id="user-name"]', "standard_user");
-    await page.fill('input[id="password"]', "secret_sauce");
-    await page.click('input[id="login-button"]');
-
+    await login(
+      page,
+      "standard_user",
+      "secret_sauce",
+      "https://www.saucedemo.com/",
+      'input[id="user-name"]',
+      'input[id="password"]',
+      'input[id="login-button"]'
+    );
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
   });
 });
